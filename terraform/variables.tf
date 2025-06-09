779aed097966305de1acbdf3208f8d83d0cd46d1
Grigor Grigoryan <g.grigoryan1296@gmail.com>
2025-06-09 21:02:03 +0400

    added checkout

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -6,7 +6,3 @@ variable "ami_id" {
   default = "ami-0fc5d935ebf8bc3bc" # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
 }
 
-variable "aws_account_id" {
-  description = "Your AWS account ID (for ECR login)"
-  type        = string
-}
